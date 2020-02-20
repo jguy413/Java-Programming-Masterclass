8e303c180ac4174e2754e1d96d17bcc977a6c739
Jonathan Guy <jguy@MatherEconomics.local>
2020-02-19 20:19:50 -0500

    Hello

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("Hello Jonny");
+
+        int myFirstNumber = 15;
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber*2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+    }
+}
